b26b4b57c8eb5b93591aa0213e676ce390047325
Rohan Thakkar <rohan27@uw.edu>
2016-11-11 22:09:11 -0800

    Pushing

---
+++ ikid/ikid/DevViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DevViewController.swift
+//  ikid
+//
+//  Created by Rohan Thakkar on 11/10/16.
+//  Copyright © 2016 Rohan Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class DevViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
